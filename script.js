eafb36e1c73071f048ba5bc86ac659f370c9977e
NuxiiGit <nuxii09@gmail.com>
2021-01-14 16:38:59 +0000

    updated site to use searchparams rather than hash

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function encode_page() {
+function encode_page_to_clipboard() {
 	var div = document.getElementById("text");
 	console.log(div.childNodes);
 	if (div != null && div.childNodes.length == 1) {
@@ -8,13 +8,9 @@ function encode_page() {
 }
 
 function decode_page() {
-	if (window.location.hash) {
-		var hash = window.location.hash.substring(1);
-		var src = decodeURIComponent(escape(atob(hash)));
-		console.log(src);
-		var div = document.getElementById("text");
-		if (div != null) {
-			div.appendChild(document.createTextNode(src));
-		}
-	}
+	var url = new URL(document.location);
+	var ciphertext = url.searchParams.get("src");
+	var plaintext = decodeURIComponent(escape(atob(ciphertext)));
+	var div = document.getElementById("text")
+	div?.appendChild(document.createTextNode(plaintext));
 }
