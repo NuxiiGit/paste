2726496bfbe236e0f119ae51f1dc13b5ce9fa103
NuxiiGit <nuxii09@gmail.com>
2021-01-14 16:31:23 +0000

    got basic page encoding working

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-function base64_encode(x) {
-	return btoa(unescape(encodeURIComponent(x)));
-}
-
-function base64_decode(x) {
-	return decodeURIComponent(escape(atob(x)));
-}
-
-if (window.location.hash) {
-	var hash = window.location.hash.substring(1);
-	var src = base64_decode(hash);
+function encode_page() {
 	var div = document.getElementById("text");
-	if (div != null) {
-		div.appendChild(document.createTextNode(src));
+	console.log(div.childNodes);
+	if (div != null && div.childNodes.length == 1) {
+		var src = div.childNodes[0].textContent;
+		return btoa(unescape(encodeURIComponent(src)));
+	}
+}
+
+function decode_page() {
+	if (window.location.hash) {
+		var hash = window.location.hash.substring(1);
+		var src = decodeURIComponent(escape(atob(hash)));
+		console.log(src);
+		var div = document.getElementById("text");
+		if (div != null) {
+			div.appendChild(document.createTextNode(src));
+		}
 	}
 }
